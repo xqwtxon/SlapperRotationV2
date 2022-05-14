c80282e84232d8edb744439708636086dd627a20
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-05-14 10:47:17 +0800

    Clean Up Codes
    “
    And you should just really clean up your code.
    I don't think they will reject you for that.
    But I'm really fricking triggered by your listener. 
    ”

--- src/Main.php
+++ src/Main.php
@@ -33,25 +33,12 @@ class Main extends PluginBase {
             $config->set("max-distance", 8);
             return;
         }
-        if (!is_int($config->get("max-distance"))){
-            $log->info("Max Distance is not string or bool. Please provide it as integer!");
-            $config->set("max-distance", 8);
-            return;
-       } 
         if ($config->get("max-distance") < 4){
             $log->info(TextFormat::RED."Your max distance is too low. Make sure your max-distance in config is not at least higher on 4!");
             $log->info("[INFO] Max-Distance was changed to 16 as default.");
             $config->set("max-distance", 8);
             return;
         }
-            if ($toggle == true){
-            $this->getServer()->getPluginManager()->registerEvents(new SlapperListener($this), $this);
-            return;
-            }
-            
-            if ($toggle == false){
-                $log->warning("The SlapperRotation is disabled by configuration.");
-            }
 		$this->saveDefaultConfig();
 	}
 }
